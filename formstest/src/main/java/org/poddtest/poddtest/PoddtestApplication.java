298615a89aa296c3c881293a65d881a2123118da
ndubikin@poddera.com <ndubikin@poddera.com>
2022-06-21 07:01:55 +0200

    Proyecto de test para Poddera Forms. Página greetings que expone un form (hardcodeado) desde un server Poddera Forms

---
+++ formstest/src/main/java/org/poddtest/poddtest/PoddtestApplication.java
@@ -0,0 +1,13 @@
+package org.poddtest.poddtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoddtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoddtestApplication.class, args);
+	}
+
+}
---
+++ formstest/src/test/java/org/poddtest/poddtest/PoddtestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.poddtest.poddtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoddtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
